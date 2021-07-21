6f4a950da557a14a4a86f100456f49810dca8390
prakash-pun7 <punbishal777@gmail.com>
2021-07-21 15:32:38 +0545

    post consume fixed

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,7 +1,37 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
 import { amqpConnect } from "config";
 import { Post } from "entity/Post";
 import { getRepository } from "typeorm";
+
+const storage = multer.diskStorage({
+  destination: function (req: Request, file: Express.Multer.File,
+    callback: (error: Error | null, destination: string) => void) {
+    const dir = './notes';
+    callback(null, dir);
+  },
+  filename: function (req: Request, file: Express.Multer.File,
+    callback: (error: Error | null, filename: string) => void) {
+    callback(null, 'notes-'+ new Date().getTime() + file.originalname);
+  }
+});
+
+const fileFilter = (req: Request, file: Express.Multer.File,
+  callback: (error: Error | null, destination: boolean) => void) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 
 const router = Router();
 
@@ -14,7 +44,13 @@ amqpConnect({ url: process.env.CLOUDAMQP_URL })
     
     channel.consume('note_created', async (message) => {
       const eventPost = JSON.parse(message.content.toString());
-      console.log(eventPost.id);
+      console.log(eventPost.result);
+      console.log(eventPost.filePath);
+      console.log(eventPost.result.title)
+      console.log(eventPost.filePath.fieldname)
+      console.log(eventPost.filePath.path)
+
+      upload.fields([eventPost.filePath]);
       
       const postRepository = getRepository(Post);
       const postData: {
@@ -26,13 +62,13 @@ amqpConnect({ url: process.env.CLOUDAMQP_URL })
         tags: string;
         content: string;
       } = {
-        originId: parseInt(eventPost.id),
-        ownerUserName: eventPost.owner.userName,
-        title: eventPost.title,
-        subTitle: eventPost.subTitle,
-        noteImage: eventPost.noteImage,
-        tags: eventPost.tags,
-        content: eventPost.content,
+        originId: parseInt(eventPost.result.id),
+        ownerUserName: eventPost.result.owner.userName,
+        title: eventPost.result.title,
+        subTitle: eventPost.result.subTitle,
+        noteImage: eventPost.filePath.path,
+        tags: eventPost.result.tags,
+        content: eventPost.result.content,
       }
       
       await postRepository.save(postData);
